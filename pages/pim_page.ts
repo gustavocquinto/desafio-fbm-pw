891426593e74b9a0fd4d523b111808b9ba546625
gustavocquinto <gutocross.kx@gmail.com>
2025-12-01 15:26:21 -0300

    feat: :sparkles: CT - Cadastro e listagem de Funcionários

--- pages/pim_page.ts
+++ pages/pim_page.ts
@@ -1,6 +1,8 @@
 import { Locator, Page, expect } from "@playwright/test";
 import { randomInt } from "crypto";
 import { Employee } from "../models/employee_model";
+import { EmployeeFactory } from "../factories/employee_factory";
+
 
 export class PimPage {
     
@@ -12,17 +14,23 @@ export class PimPage {
     readonly employeeIdInput: Locator;
     readonly addEmployeeSaveButton: Locator; 
     readonly pimLinkButton: Locator;
+    readonly employeeListButton: Locator;
+    readonly searchByFilterButton: Locator;
+    readonly employeeNameInput: Locator;
 
     constructor(page: Page){
         this.page = page;
 
-        this.addButton = this.page.getByRole('button', {name: 'Add'});
+        this.addButton = this.page.getByRole('link', {name: 'Add Employee'});
         this.firstNameInput = this.page.getByPlaceholder("First Name");
         this.middleNameInput = this.page.getByPlaceholder("Middle Name");
         this.lastNameInput = this.page.getByPlaceholder("Last Name");
         this.employeeIdInput = this.page.locator('.oxd-input-group:has(label:has-text("Employee Id")) input');
         this.addEmployeeSaveButton = this.page.getByRole('button', {name: 'save'});
         this.pimLinkButton = this.page.getByRole('link', { name: 'PIM' });
+        this.employeeListButton = this.page.getByRole('link', { name: 'Employee List'});
+        this.searchByFilterButton = this.page.getByRole('button', {name: 'Search'});
+        this.employeeNameInput = this.page.locator('.oxd-input-group:has(label:has-text("Employee Name")) input');
     }
 
     async accessPimPage(){
@@ -36,6 +44,58 @@ export class PimPage {
         await this.lastNameInput.fill(employee.lastName);
         await this.employeeIdInput.fill(employee.id.toString());
         await this.addEmployeeSaveButton.click();
-
     }
+
+    async accessEmployeeList(){
+        await this.employeeListButton.click();
+    }
+
+    async findEmployeeByFilter(filterLabel: string, value: string){
+
+        switch(filterLabel){
+            case 'Employee Id':
+                await this.employeeIdInput.fill(value)
+            case 'Employee Name':
+                await this.employeeNameInput.fill(value)
+        }
+
+        await this.searchByFilterButton.click();
+    }
+    
+    async findEmployeeById(employeeId: String){
+        await this.employeeIdInput.fill(employeeId.toString())
+        await this.searchByFilterButton.click();
+        await this.page.waitForTimeout(2500);
+    }
+
+    async deleteEmployee(): Promise<String>{
+        let employeeId = '';
+        await this.page.waitForTimeout(2500);
+
+        // pego linha da tabela
+        const rows = await this.page.locator('.oxd-table-card').all();
+        const secondRow = rows[1];
+        const cells = await secondRow.locator('[role="cell"]');
+
+        // capturo o ID do funcionário
+        employeeId = await cells.nth(1).innerText();
+        console.log("Número do employee: " + employeeId.toString());
+
+        const deleteButton = await secondRow.locator('.oxd-table-cell-actions button').nth(1);
+        await deleteButton.click();
+
+        // confirmo a exclusão no modal
+        const confirmBtn = await this.page.getByRole('button', { name: 'Yes, Delete' });
+        if (await confirmBtn.isVisible()) {
+            await confirmBtn.click();
+        }
+
+        //retorno o id do employee excluído
+        return employeeId;
+    }
+
+    async tableRows(){
+        return await this.page.locator('.oxd-table-card');
+    }
+
 }
--- tests/pim.spec.ts
+++ tests/pim.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '../fixtures/login_fixture';
 import { PimPage } from '../pages/pim_page';
+import { Toast } from '../pages/toasts_page';
 import { EmployeeFactory } from '../factories/employee_factory'
+
 
 test.describe('PIM', () => {
     const employeeFactory = new EmployeeFactory();
@@ -11,29 +13,66 @@ test.describe('PIM', () => {
         await pimPage.accessPimPage();
     })
 
-    test('CT-PIM-001 - Criar colaborador', async ({page}) => {
+    test('CT-PIM-001 - Cadastro e listagem de Funcionários', async ({page}) => {
+        let employees = [];
+        const pimPage = new PimPage(page)
+        const toastPage = new Toast(page)
 
+        //Criar 3 Employees
         for(let i = 0; i < 3; i++){
-            const pimPage = new PimPage(page)
-
             const employee = employeeFactory.employee("Funcionario", "teste", i.toString())
+            employees.push(employee);
 
             await pimPage.addEmployee(employee)
 
             // Mensagens de Sucesso
-            await expect(page.getByText('Success', {exact: true})).toBeVisible();
-            await expect(page.getByText('Successfully Saved', {exact: true})).toBeVisible();
+            const toast = await toastPage.getSuccessToast();
+            await expect(toast.message).toContain('Successfully Saved')
 
             // Valores preenchidos corretamente no detalhamento de Employee recém cadastrado
             await expect(pimPage.firstNameInput).toHaveValue(employee.firstName);
             await expect(pimPage.middleNameInput).toHaveValue(employee.middleName);
             await expect(pimPage.lastNameInput).toHaveValue(employee.lastName);
         }
+
+        //Acesso a listagem de Employee
+        await pimPage.accessEmployeeList();
+
+        //Consulto employee utilizando filtragem por Id recém criado
+        for (const employee of employees){
+            
+            await pimPage.findEmployeeById(employee.id.toString());
+
+            //Employee aparece na lista com os valores previamente cadastrados
+            const employeefirstMiddleName = `${employee.firstName} ${employee.middleName}`
+
+            await expect(page.getByRole('cell', { name: employee.id.toString(), exact: true })).toBeVisible();
+            await expect(page.getByRole('cell', { name: employeefirstMiddleName, exact: true })).toBeVisible();
+            await expect(page.getByRole('cell', { name: employee.lastName, exact: true})).toBeVisible();
+        }
     });
 
-    test('CT-PIM-002 - Validar lista de funcionários', async ({page}) => {
-        
-    })
+    test('CT-PIM-002 - Remover dois funcionários', async ({page}) =>{
+        const pimPage = new PimPage(page);
+        const toastPage = new Toast(page);
 
+        await pimPage.accessEmployeeList();
+
+        for (let i = 0; i < 1; i++){
+             const deletedEmployeeId = await pimPage.deleteEmployee();
+
+             const successToast = await toastPage.getSuccessToast();
+             await expect(successToast.message).toContain('Successfully Deleted')
+
+             await page.waitForSelector('.oxd-toast', { state: 'detached' });
+             await pimPage.findEmployeeById(deletedEmployeeId);
+
+             const infoToast = await toastPage.getInfoToast()
+             await expect(infoToast.message).toContain('No Records Found')
+             await expect((await pimPage.tableRows())).toHaveCount(0)
+             await page.waitForSelector('.oxd-toast', { state: 'detached' });
+             
+        }
+    })
 
 });
